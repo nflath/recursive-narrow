a4b611a71a7d491eb569e74864f339da34b0b3d2
Nathaniel Flath <flat0103@gmail.com>
2014-08-01 12:47:13 -0700

    Add 'dwim' function

--- recursive-narrow.el
+++ recursive-narrow.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nathaniel Flath <flat0103@gmail.com>
 ;; URL: http://github.com/nflath/recursive-narrow
-;; Version: 1.0
+;; Version: 1.2
 
 ;; This file is not part of GNU Emacs.
 
@@ -22,8 +22,8 @@
 ;; to your .emacs file:
 ;;
 ;; (require 'recursive-narrow)
-;; (global-set-key (kbd "C-x n n") 'recursive-narrow-to-region)
-;; (global-set-key (kbd "C-x n w") 'recursive-widen)
+;; (global-set-key (kbd "C-x n n") 'recursive-narrow-or-widen-dwim)
+;; (global-set-key (kbd "C-x n w") 'recursive-widen-dwim)
 ;;
 
 ;;; License:
@@ -46,29 +46,79 @@
 ;;; Code:
 
 (setq recursive-narrow-buffers nil)
+(setq recursive-narrow-to nil)
+(setq recursive-narrow-in nil)
+(require 'cl)
+
+(defun recursive-narrow-or-widen-dwim ()
+  "If the region is active, narrow to that regoin.
+Otherwise, narrow to the current function
+or org subtree.  If this has no effect, widen the buffer."
+  (interactive)
+  (let ((old-recursive-narrow-buffers (list-length recursive-narrow-buffers))
+        (recursive-state-changed t))
+    (cond ((region-active-p) (recursive-narrow-to-region (region-beginning) (region-end)))
+          ((derived-mode-p 'prog-mode) (message "narrowing to defun") (narrow-to-defun))
+          ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+          (t (setq recursive-state-changed nil)))
+    (if (or (not recursive-state-changed)
+            (= (list-length recursive-narrow-buffers) old-recursive-narrow-buffers))
+        (progn
+          (print (list-length recursive-narrow-buffers))
+          (recursive-widen)))))
 
 (defun recursive-narrow-to-region (start end)
-  """Replacement of narrow-to-region.  Performs the exact same
-  function but also allows recursive-widen to remove just one
-  call to recursive-narrow-to-region."""
+  "Replacement of `narrow-to-region'.
+Performs the exact same
+function but also allows `recursive-widen' to remove just one
+  call to `recursive-narrow-to-region'.
+Argument START beginning of region.
+Argument END end of region."
   (interactive "r")
-  (setq recursive-narrow-buffers
-        (append (list (cons (current-buffer) (cons (point-min) (point-max))))
-                recursive-narrow-buffers ))
-  (narrow-to-region start end))
+  (let
+      ((recursive-narrow-in t)
+       (current-narrow-settings (assoc (current-buffer) recursive-narrow-to)))
+    (unless (and current-narrow-settings
+             (= (car (cdr current-narrow-settings)) start)
+             (= (cdr (cdr current-narrow-settings)) end))
+      (setq recursive-narrow-buffers
+            (append (list (cons (current-buffer) (cons (point-min) (point-max))))
+                    recursive-narrow-buffers))
+      (setq recursive-narrow-to
+            (append (list (cons (current-buffer) (cons start end))
+                    recursive-narrow-to)))
+      (narrow-to-region start end))))
 
 (defun recursive-widen ()
-  """Replacement of widen that will only pop one level of visibility."""
+  "Replacement of widen that will only pop one level of visibility."
   (interactive)
-  (if (assoc (current-buffer) recursive-narrow-buffers)
-      (progn
-        (setq widen-to (cdr (assoc (current-buffer) recursive-narrow-buffers)))
-        (setq recursive-narrow-buffers (remove
-                                        (assoc (current-buffer) recursive-narrow-buffers)
-                                        recursive-narrow-buffers))
-        (narrow-to-region (car widen-to) (cdr widen-to))
-        (recenter))
-    (widen)))
+  (let ((recursive-narrow-in t))
+    (if (assoc (current-buffer) recursive-narrow-buffers)
+        (progn
+          (setq widen-to (cdr (assoc (current-buffer) recursive-narrow-buffers)))
+          (setq recursive-narrow-buffers (remove
+                                          (assoc (current-buffer) recursive-narrow-buffers)
+                                          recursive-narrow-buffers))
+          (setq recursive-narrow-to (remove
+                                     (assoc (current-buffer) recursive-narrow-to)
+                                          recursive-narrow-to))
+          (narrow-to-region (car widen-to) (cdr widen-to))
+          (recenter))
+      (widen))))
+
+(defadvice narrow-to-region (around potentially-recursive activate)
+  (message "advice of narrow to region")
+  (if recursive-narrow-in
+      ad-do-it
+    (recursive-narrow-to-region (ad-get-arg 0) (ad-get-arg 1))))
+
+(defadvice widen (around potentially-recursive activate)
+  (if recursive-narrow-in
+      ad-do-it
+    (recursive-widen)))
+
+(global-set-key (kbd "C-x n w") 'recursive-widen)
+(global-set-key (kbd "C-x n n") 'recursive-narrow-or-widen-dwim)
 
 (provide 'recursive-narrow)
 
